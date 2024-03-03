6271a55639ba7c5714922d6294a17ec5ad1ea3e3
tharinm <tharindumadhushan864@gmal.com>
2024-03-03 16:33:11 +0530

    array object

--- src/example.js
+++ src/example.js
@@ -1,5 +1,17 @@
-function sum(a, b) {
-    return a + b;
-}
-var result = sum(4, 5);
-console.log(result);
+//primmitive types
+var age;
+age = 4;
+var user;
+user = "John Doe";
+var instructor;
+instructor: true;
+var hobby;
+//more complex types
+var hobbies;
+hobbies = ["Sports", "Cooking"];
+var person;
+person = {
+    name: "Max",
+    age: 23,
+};
+var people;
--- src/example.ts
+++ src/example.ts
@@ -1,11 +1,32 @@
 //primmitive types
-let age: number
-age = 4
+let age: number;
+age = 4;
 
-let user: string
-user = 'John Doe'
+let user: string;
+user = "John Doe";
 
-let instructor: boolean
-instructor: true
+let instructor: boolean;
+instructor: true;
 
-let hobby: null
+let hobby: null;
+
+//more complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 23,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
